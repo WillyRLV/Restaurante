c43aa68051a81ecb143a5e99e99148fe06868cbe
Jean Paul <jeanpaulbenitescoico@gmail.com>
2022-12-27 15:43:30 -0500

    Envio1

--- main.js
+++ main.js
@@ -8,3 +8,19 @@
 //     return mobil;
 // }
 //==============================
+
+//Codigo gs JP
+//------------------------
+function doGet() {
+     var template = HtmlService.createTemplateFromFile('interfaz-cartas')
+     let output = template.evaluate();
+     let mobil = HtmlService.createHtmlOutput(output);
+     mobil.addMetaTag('viewport', 'width=device-width, initial-scale=1');  
+    return mobil;
+}
+
+//============
+    
+function include(filename){
+    return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
