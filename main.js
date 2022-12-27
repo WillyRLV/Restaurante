14d7471ee591373b3f9f5b359a1c2c7c6e841a8c
William <Williamlv1998@gmail.com>
2022-12-27 12:49:57 -0500

    proyecto base

---
+++ main.js
@@ -0,0 +1,10 @@
+//aquí codigo identificado
+//doget prueba
+// function doGet() {
+//     var template = HtmlService.createTemplateFromFile('index')
+//     let output = template.evaluate();
+//     let mobil = HtmlService.createHtmlOutput(output);
+//     mobil.addMetaTag('viewport', 'width=device-width, initial-scale=1');  
+//     return mobil;
+// }
+//==============================
