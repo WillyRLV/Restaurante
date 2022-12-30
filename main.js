53f5b23f9c00866f5608c87a4c059820d03c7672
William <Williamlv1998@gmail.com>
2022-12-30 13:54:19 -0500

    agregando tarjetas dinamicas en tiempo real

--- main.js
+++ main.js
@@ -10,94 +10,133 @@
 //==============================
 
 //ramawill
-function setAutomatic(){
+function setAutomatic() {
     var id = "1e3W4zaDx8ByhuWEKyBTx9FwTQa_-jW1MnrGYRO3nol0";
-   var ss = SpreadsheetApp.openById(id);
-   var data = ss.getSheetByName("base");
-   let val1 = data.getRange("A2").getValue();
-   let val2 = data.getRange('B2').getValue();
- 
-  return [val1,val2]
- 
- }
+    var ss = SpreadsheetApp.openById(id);
+    var data = ss.getSheetByName("base");
+    let val1 = data.getRange("A2").getValue();
+    let val2 = data.getRange('B2').getValue();
 
- //test
- function dataFood() {
- 
-     var id = "16i_ZO2y0K4umabI3bJDPFm01RilUpwjlu1pT3hCdwZU";
-     var ss = SpreadsheetApp.openById(id);
-     var hojaUsuarios = ss.getSheetByName("Comidas");
-     var datos = hojaUsuarios.getDataRange().getValues();
-     datos.shift();
-     var obj = {}
-     var datoe = []
-       // const valor1 = '95b9f5f3f74dc330b57a9bfbc74be654'
- 
-     //========================
-     //con foreach
-      datos.map((dato) => {
-       
-          const proyectos = 
-            {nomb: dato[1],
+    return [val1, val2]
+
+}
+
+//test
+function dataFood() {
+
+    var id = "16i_ZO2y0K4umabI3bJDPFm01RilUpwjlu1pT3hCdwZU";
+    var ss = SpreadsheetApp.openById(id);
+    var hojaUsuarios = ss.getSheetByName("Comidas");
+    var datos = hojaUsuarios.getDataRange().getValues();
+    datos.shift();
+    var obj = {}
+    var datoe = []
+    // const valor1 = '95b9f5f3f74dc330b57a9bfbc74be654'
+
+    //========================
+    //con foreach
+    datos.map((dato) => {
+
+        const proyectos =
+        {
+            nomb: dato[1],
             img: dato[2],
             dec: dato[3],
-            prec:dato[4]
-          }
-          
-           datoe.push(proyectos)
-             obj = { status: "200", proyectos: datoe  }
-     })
-      
-   
-  return obj
-   }
+            prec: dato[4]
+        }
+
+        datoe.push(proyectos)
+        obj = { status: "200", proyectos: datoe }
+    })
+
+
+    return obj
+}
+//comandas
+function dataCom() {
+
+    var id = "16i_ZO2y0K4umabI3bJDPFm01RilUpwjlu1pT3hCdwZU";
+    var ss = SpreadsheetApp.openById(id);
+    var hojaUsuarios = ss.getSheetByName("Comandas");
+    var datos = hojaUsuarios.getDataRange().getValues();
+    datos.shift();
+    var obj = {}
+    var datoe = []
+    // const valor1 = '95b9f5f3f74dc330b57a9bfbc74be654'
+
+    //========================
+    //con foreach
+    datos.map((dato) => {
+
+        const proyectos =
+        {
+            nombre: dato[1],
+            apellido: dato[2],
+            distrito: dato[3],
+            direccion: dato[4],
+            telefono: dato[5],
+            referencia: dato[6],
+            comentario: dato[7],
+            pedidos: dato[8],
+            status: dato[9],
+            fecha: dato[10],
+            hora: dato[11]
+        }
+
+        datoe.push(proyectos)
+        obj = { status: "200", proyectos: datoe }
+    })
+
+
+    return obj
+}
 //=========================
 
 //Codigo gs JP
 //------------------------
 function doGet(e) {
-    if(e.parameter.page){
-      var pageName = e.parameter.page.trim().toLowerCase();
-      if (pageName !== "index_login"){
-        var template = HtmlService.createTemplateFromFile(pageName);
-        template.url = getPageUrl();
-        var output = template.evaluate();
-        var htmlOutput = HtmlService.createHtmlOutput(output);   
-       htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1');   
-        return htmlOutput;
-      }else{
+    if (e.parameter.page) {
+        var pageName = e.parameter.page.trim().toLowerCase();
+        if (pageName !== "index_login") {
+            var template = HtmlService.createTemplateFromFile(pageName);
+            template.url = getPageUrl();
+            var output = template.evaluate();
+            var htmlOutput = HtmlService.createHtmlOutput(output);
+            htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+            return htmlOutput;
+        } else {
+            return homePage();
+        }
+    } else {
         return homePage();
-      }
-    }else{
-      return homePage();
     }
-  }
-  
-  
-  function homePage(){
-    var pages = ['interfaz','prueba'];
-  var urls = pages.map(function(name){
-   return getPageUrl(name);
-  });
-  var template = HtmlService.createTemplateFromFile("index_login");
-  template.test = urls;
-  var output = template.evaluate();
-  var htmlOutput = HtmlService.createHtmlOutput(output);   
-    htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1');   
-  
-  return htmlOutput;
-  }
-  
-  
-  function getPageUrl(name){
-    if (name){
-      var url = ScriptApp.getService().getUrl();
-      return url + "?page=" + name;
-    }else{
-      return ScriptApp.getService().getUrl();
+}
+
+
+function homePage() {
+    var pages = ['interfaz', 'pizarra'];
+    var urls = pages.map(function (name) {
+        return getPageUrl(name);
+    });
+    var template = HtmlService.createTemplateFromFile("index_login");
+    template.test = urls;
+    var output = template.evaluate();
+    var htmlOutput = HtmlService.createHtmlOutput(output);
+    htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+
+    return htmlOutput;
+}
+
+
+function getPageUrl(name) {
+    if (name) {
+        var url = ScriptApp.getService().getUrl();
+        return url + "?page=" + name;
+    } else {
+        return ScriptApp.getService().getUrl();
     }
-  }
-  
+}
+
 
 
 //============
